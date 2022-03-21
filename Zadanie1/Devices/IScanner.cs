85ee6a623233916673754c0c6f8c1dc254495950
David Kuc <david.kuc@microsoft.wsei.edu.pl>
2022-03-21 19:20:35 +0100

    Init commit

---
+++ Zadanie1/Devices/IScanner.cs
@@ -0,0 +1,6 @@
+﻿public interface IScanner : IDevice
+{
+    // dokument jest skanowany, jeśli urządzenie włączone
+    // w przeciwnym przypadku nic się dzieje
+    void Scan(out IDocument document, IDocument.FormatType formatType);
+}
---
+++ Zadanie1/Documents/ImageDocument.cs
@@ -0,0 +1,6 @@
+﻿
+public class ImageDocument : AbstractDocument
+{
+    public ImageDocument(string filename) : base(filename) { }
+    public override IDocument.FormatType GetFormatType() => IDocument.FormatType.JPG;
+}
