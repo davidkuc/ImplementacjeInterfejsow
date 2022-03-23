01883fe4b6aaa49acc264f3df348923dc5b8cbf0
David Kuc <david.kuc@microsoft.wsei.edu.pl>
2022-03-23 14:04:12 +0100

    Added few lines to Program.cs

--- Zadanie3/Program.cs
+++ Zadanie3/Program.cs
@@ -24,3 +24,8 @@ multidimensionalDevice.AllDevicesPowerOff();
 multidimensionalDevice.Send(doc);
 multidimensionalDevice.FaxPowerOn();
 multidimensionalDevice.Send(doc);
+
+Console.WriteLine();
+Console.WriteLine($"Scan count: {multidimensionalDevice.ScanCounter}");
+Console.WriteLine($"Print count: {multidimensionalDevice.PrintCounter}");
+Console.WriteLine($"Send count: {multidimensionalDevice.SendCounter}");
